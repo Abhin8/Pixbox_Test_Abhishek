368919784911be3941d3539ad0db549edacbfaf8
abhishek <abhishek@maninfini.com>
2023-05-23 09:13:22 +0530

    implement clicj button

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import logo from "./logo.svg";
 import "./App.css";
+import Button from "react-bootstrap/Button";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
-  return <div className="App">Hello World</div>;
+  return (
+    <div className="App">
+      <Button>Click me</Button>
+    </div>
+  );
 }
 
 export default App;
